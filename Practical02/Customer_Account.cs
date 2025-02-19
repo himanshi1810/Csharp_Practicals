a8ed0cf8c3860de50f16ebcce8b9cefa3b6f91be
himanshi1810 <hgandhi1810@gmail.com>
2025-02-19 09:14:00 +0530

    Practical02 in Practical 02 branch

--- Practical02/Customer_Account.cs
+++ Practical02/Customer_Account.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Practical02
 {
     //1st step :- Define the class
